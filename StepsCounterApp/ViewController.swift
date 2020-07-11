2ad57cda9f2f6418c57ea26702c8cbb8567ba35f
HG <h.garabedian@outlook.com>
2020-07-10 20:32:05 -0700

    Initial Commit

---
+++ StepsCounterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StepsCounterApp
+//
+//  Created by GGTECH LLC on 7/10/20.
+//  Copyright © 2020 GGTECH LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
